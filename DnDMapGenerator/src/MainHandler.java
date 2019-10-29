a475f700db66ff19144844d425fc23d81a449ec1
John Avent <john.avent@protonmail.com>
2019-10-29 17:01:13 -0500

    New Netbeans Project

---
+++ DnDMapGenerator/src/MainHandler.java
@@ -0,0 +1,14 @@
+/**
+ *
+ * @author John Avent
+ */
+public class MainHandler {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
