46d47d41d3ec8db057e67f303e9d9712d73a11b2
John Avent <john.avent@protonmail.com>
2019-11-02 15:29:42 -0500

    Created stubs for all classes except views.

---
+++ MapGenerator/src/mapgenerator/MapTile.java
@@ -0,0 +1,53 @@
+package mapgenerator;
+
+/**
+ *
+ * @author CS 321 Team 1
+ */
+public class MapTile implements MapCoordinates
+{
+    private int xCoord;
+    private int yCoord;
+    private int terrainIndex;
+    
+    public MapTile(int newXCoord, int newYCoord, int newTerrainIndex)
+    {
+        xCoord = newXCoord;
+        yCoord = newYCoord;
+        terrainIndex = newTerrainIndex;
+    }
+            
+    @Override
+    public int getXCoord()
+    {
+        return xCoord;
+    }
+    
+    @Override
+    public int getYCoord()
+    {
+        return yCoord;
+    }
+    
+    public int getTerrainIndex()
+    {
+        return terrainIndex;
+    }
+    
+    @Override
+    public void setXCoord(int newX)
+    {
+        xCoord = newX;
+    }
+    
+    @Override
+    public void setYCoord(int newY)
+    {
+        yCoord = newY;
+    }
+    
+    public void setTerrainIndex(int newTerrainIndex)
+    {
+        terrainIndex = newTerrainIndex;
+    }
+}
