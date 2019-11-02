c9a0987931d697d65e25f1adf4d71bf2f2fadbde
John Avent <john.avent@protonmail.com>
2019-11-02 15:30:04 -0500

    Created stubs for all classes except views.

---
+++ MapGenerator/src/mapgenerator/MapCoordinates.java
@@ -0,0 +1,14 @@
+package mapgenerator;
+
+/**
+ *
+ * @author CS 321 Team 1
+ */
+public interface MapCoordinates 
+{
+    int getXCoord();
+    int getYCoord();
+    
+    void setXCoord(int newX);
+    void setYCoord(int newY);
+}
