bfb8fec7c36afc4e409f23218da700fc7539e292
John Avent <john.avent@protonmail.com>
2019-11-02 15:30:21 -0500

    Created stubs for all classes except views.

---
+++ MapGenerator/src/mapgenerator/MainHandler.java
@@ -0,0 +1,17 @@
+package mapgenerator;
+
+/**
+ *
+ * @author CS 321 Team 1
+ */
+public class MainHandler 
+{
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        // TODO code application logic here
+    }
+    
+}
