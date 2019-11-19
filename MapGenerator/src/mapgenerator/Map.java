19859be59a87a9aed4bb0a05b76e520fe4f0c41c
KuatYards <55962458+KuatYards@users.noreply.github.com>
2019-11-19 15:26:04 -0600

    Update Map.java
    added initial map generation algorithms

--- MapGenerator/src/mapgenerator/Map.java
+++ MapGenerator/src/mapgenerator/Map.java
@@ -13,39 +13,94 @@ public class Map
     private int numCols;
     private int numRows;
     private int biomeIndex;
+        java.util.Random rand = new java.util.Random(); // random number generator base function
+    
+    private double meanTile = 0.0;       // the average tile type 
+    private double terrainClutter = 0.3; // the amount of clutter the terrain has
     
     private void generateMapTileList()
     {
-        // List<List<String>> listOfLists = new ArrayList<List<String>>(size); 
-        mapTileList = new ArrayList<ArrayList<MapTile>>(); 
-        for(int i = 0; i < numCols; i++)  {
-            mapTileList.add(new ArrayList<MapTile>());
-        }
-        for (int x = 0; x < numCols; x++)
-        { for (int y = 0; y < numRows; y++)
-            {
-                int terrainIndex = 0;
-                MapTile newMapTile = new MapTile(x, y, terrainIndex);
-                mapTileList.get(x).add(newMapTile);
+        mapTileList = new ArrayList<>(numCols * numRows);
+        int j = 0;
+        for(int i = 0; i < numCols * numRows; i++)
+        {
+            int x = i % numRows;
+            int y = i / numRows;
+            int terrainIndex = 0;
+            
+            
+            
+            // use gaussian noise generator for open biomes
+            double noise = rand.nextGaussian() * Math.sqrt(terrainClutter) + meanTile;
+            noise = Math.abs(noise);
+            if (noise < 0.5) { terrainIndex = 0; }
+            if (noise > 0.5 && noise < 1.0) { terrainIndex = 1; }
+            if (noise > 1.0 && noise < 1.5) {terrainIndex = 2; }
+            System.out.print(terrainIndex);
+            j = j + 1;
+            if (j >= numCols) { 
+                System.out.print('\n');
+                j = 0; 
             }
+            MapTile newMapTile = new MapTile(x, y, terrainIndex);
+            mapTileList.add(newMapTile);
+        }
+        if (biomeIndex == 2) {
+                
+                double doubleCols = (double)numCols; // cast variables to doubles
+                double doubleRows = (double)numRows;
+                
+                int curX = 0; // current index in the data arrays
+                int curY = 0;
+                
+                int[][] starts = new int[numCols*numRows][2]; // build data arrays
+                int[][] ends = new int[numCols*numRows][2];
+                
+                int startCol = (int)Math.round(rand.nextDouble()*doubleCols); // create starting point
+                int startRow = (int)Math.round(rand.nextDouble()*doubleRows);
+                
+                starts[curX][0] = startCol; // fill starting point in data arrays
+                starts[curX][1] = startRow;
+                
+                int col = startCol;
+                int row = startRow;
+                // begin loop
+                while(curX < (numCols*numRows)-2){              // loop through data arrays
+                    curX++; // increment x value
+                    
+                    double dir = rand.nextDouble() * 4.0;       // choose direction
+                    if (dir < 1.0) {col = col + 1;}                // North
+                    if (dir >= 1.0 && dir < 2.0) {col = col - 1;}  // South
+                    if (dir >= 2.0 && dir < 3.0) {row = row + 1;}  // East
+                    if (dir >= 3.0) {row = row - 1;}               // West
+                    
+                    ends[curX][0] = col;       // fill current ends with the new coordinate
+                    ends[curX][1] = row;
+                    
+                    starts[curX+1][0] = col;   // fill next start with current coordinate
+                    starts[curX+1][1] = row;
+                }
         }
     }
     
-    public Map(int newNumCols, int newNumRows, int newBiomeIndex)
+    public Map(int newNumCols, int newNumRows, int newBiomeIndex, double mean, double clutter)
     {
         numCols = newNumCols;
         numRows = newNumRows;
         biomeIndex = newBiomeIndex;
+        meanTile = mean;
+        terrainClutter = clutter;
         generateMapTileList();
     }
     
-    //I dont understand this usecase scenario -- Jake
-    public void generateNewMap(int newNumCols, int newNumRows, int newBiomeIndex)
+    public void generateNewMap(int newNumCols, int newNumRows, int newBiomeIndex, double mean, double clutter)
     {
         numCols = newNumCols;
         numRows = newNumRows;
         biomeIndex = newBiomeIndex;
         mapTileList.clear();
+        meanTile = mean;
+        terrainClutter = clutter;
         generateMapTileList();
     }
     
@@ -89,10 +144,22 @@ public class Map
         numRows = newNumRows;
     }
     
-    public void setBiomeIndex(int newBiomeIndex)
+        public void setBiomeIndex(int newBiomeIndex)
     {
         biomeIndex = newBiomeIndex;
     }
+    public double getTerrainClutter() {
+        return terrainClutter;
+    }
+    public void setTerrainClutter(double newTerrainClutter) {
+        terrainClutter = newTerrainClutter;
+    }
+    public double getTerrainAverage() {
+        return meanTile;
+   }
+   public void setTerrainAverage(double newMeanTile) {
+       meanTile = newMeanTile;
+   }
     public void setMap(ArrayList<ArrayList<MapTile>> newMap)
     {
         mapTileList = newMap;
